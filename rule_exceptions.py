6d1aef1ef2e410d21ab862feca6338b0dc8dbe22
meginks <meganjones.dev@gmail.com>
2024-09-16 12:35:53 -0700

    edited comment

--- rule_exceptions.py
+++ rule_exceptions.py
@@ -2,7 +2,7 @@
 
 # Exceptions to the rule for creating stems for FUTURE and CONDITIONAL tenses -- there are relatively few of these in Spanish so it is more practical to store these than to derive a pattern that may not be accurate 
 
-future_cond_exceptions = { #TO DO -- see if derived forms of these verbs also follow this pattern
+future_cond_exceptions = { #TO DO -- other derived forms of these verbs also follow this pattern
     'caber': 'cabr', 
     'decir': 'dir', 
     'haber': 'habr', 
