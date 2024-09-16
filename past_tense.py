6867505bf03a83d3388cefbcd935bf6753d672c3
meginks <meganjones.dev@gmail.com>
2024-09-16 14:05:32 -0700

    added imperfect subjunctive

--- past_tense.py
+++ past_tense.py
@@ -107,39 +107,33 @@ def ellos_preterito(infinitive):
 
 ################# SUBJUNTIVO ########################## 
 
-# tú in imperfect subjunctive 
+# yo, el, ella, usted in imperfect subjunctive 
 # PARAMETER : STRING infinitive form 
 # RETURN TYPE: STRING conjugated form  
-def tu_subjuntivo_pasado(infinitive): 
-    conjugated_form = '' 
+def yo_el_subjuntivo_pasado(infinitive): 
+    conjugated_form = get_3_pers_pl_preterite_stem(infinitive) + 'a'
     return conjugated_form 
 
 # tú in imperfect subjunctive 
 # PARAMETER : STRING infinitive form 
 # RETURN TYPE: STRING conjugated form  
 def tu_subjuntivo_pasado(infinitive): 
-    conjugated_form = '' 
-    return conjugated_form 
-
-# él / ella / usted in subjunctive past
-# PARAMETER : STRING infinitive form 
-# RETURN TYPE: STRING conjugated form  
-def el_subjuntivo_pasado(infinitive): 
-    conjugated_form = '' 
+    conjugated_form = yo_el_subjuntivo_pasado(infinitive) + 's'
     return conjugated_form 
 
 # nosotros / nosotras in imperfect subjunctive 
 # PARAMETER : STRING infinitive form 
 # RETURN TYPE: STRING conjugated form  
-def nosotros_subjuntivo_pasado(infinitive): 
-    conjugated_form = '' 
+def nosotros_subjuntivo_pasado(infinitive):  
+    # MIGHT NEED TO ADJUST THIS FOR ACCENT MARK PURPOSES 
+    conjugated_form = yo_el_subjuntivo_pasado(infinitive) + 'amos'
     return conjugated_form 
 
 # vosotros / vosotras in imperfect subjunctive 
 # PARAMETER : STRING infinitive form 
 # RETURN TYPE: STRING conjugated form  
 def vosotros_subjuntivo_pasado(infinitive): 
-    conjugated_form = '' 
+    conjugated_form = yo_el_subjuntivo_pasado(infinitive) + 'n' 
     return conjugated_form 
 
 # ellos / ellas / ustedes in imperfect subjunctive 
